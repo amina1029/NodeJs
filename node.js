97ff301fca9350f6d6570264084fb74d80913caf
amina1029 <106466021+amina1029@users.noreply.github.com>
2023-04-26 23:28:26 +0500

    Add files via upload

--- node.js
+++ node.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 foldername = "test";
+//Create  folder
 if(!fs.existsSync(foldername)){
 fs.mkdir(foldername, (err)=>{
     if(err)
@@ -8,26 +9,31 @@ fs.mkdir(foldername, (err)=>{
     return;
 })
 }
+//create file
 filename = "index.text";
 const filepath= path.join(foldername,filename);
 fs.writeFile(filepath, "hello",(err)=>{
     if(err)
     console.log("show error" , err);
 })
+//Append data to file
 fs.appendFile(filepath,"my name is amina", (err)=>{
     if(err)
     console.log(err);
 })
+//read data from file
  fs.readFile(filepath,(err,data)=>{
     if(err)
     console.log("show error",'utf8' , err);
     else
     console.log(data.toString());
 })
+//Rename file
 const newfilepath = path.join(foldername,"code.text");
 fs.rename(filepath, newfilepath,(err)=>
 {
     if(err)
     console.log(err);
 })
-//fs.unlinkSync(filepath)
+//Delete file
+fs.unlinkSync(filepath)
